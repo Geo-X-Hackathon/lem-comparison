8ef5b593319307e4d5aeb6b466c6a393ae8b5644
Fiona Clubb <f.clubb@ed.ac.uk>
2018-03-13 16:49:47 +0100

    Taylor plot visualisation and colourmaps

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from differences import *
 from input_dem import *
 from correlation import *
 from taylorDiagram import *
-
+from glob import glob
 
 # define the filenames
 DataDirectory = '../Caesar-Ddata/'
@@ -30,24 +30,68 @@ print(RefStd)
 # now for each other DEM, calculate the pearson correlation and standard deviation
 
 #create figure
-fig = plt.figure(figsize=(10, 4))
-dia = TaylorDiagram(RefStd, fig=fig, rect=122, label="Reference")
+fig = plt.figure(figsize=(10, 5))
+dia = TaylorDiagram(RefStd, fig=fig, rect=121, label="Reference")
+
+dia2 = TaylorDiagram(RefStd, fig=fig, rect=122, label="Reference")
 
 # Add grid
 dia.add_grid()
+dia2.add_grid()
 
 # Add RMS contours, and label them
 contours = dia.add_contours(colors='0.5')
 PLT.clabel(contours, inline=1, fontsize=10, fmt='%.2f')
+contours = dia2.add_contours(colors='0.5')
+PLT.clabel(contours, inline=1, fontsize=10, fmt='%.2f')
 
-pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS1,InitialDEM,RefDod)
-dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='r', mec='k')
+# for each model run, get all the files and plot them onto the diagram
 
-pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS2,InitialDEM,RefDod)
-dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='g', mec='k')
+# D1
+files = sorted(glob(DataDirectory+'D1*.txt'))
+n_files = len(files)
+colors = plt.matplotlib.cm.Reds(np.linspace(0.1, 0.9, n_files))
+for i, DEMFileName in enumerate(files):
+    print DEMFileName
+    pointcp, pointsd = CalculateTaylorPoint(DEMFileName,InitialDEM,RefDod)
+    dia.add_sample(pointsd, pointcp, marker='o', ms=10, ls='', mfc=colors[i], mec='k')
 
-pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS3,InitialDEM,RefDod)
-dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='b', mec='k')
+# D3
+files = sorted(glob(DataDirectory+'D3*.txt'))
+colors = plt.matplotlib.cm.Blues(np.linspace(0.1, 0.9, n_files))
+for i, DEMFileName in enumerate(files):
+    pointcp, pointsd = CalculateTaylorPoint(DEMFileName,InitialDEM,RefDod)
+    dia.add_sample(pointsd, pointcp, marker='o', ms=10, ls='', mfc=colors[i], mec='k')
 
-pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS4,InitialDEM,RefDod)
-dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='k', mec='r')
+# D2
+files = sorted(glob(DataDirectory+'D2*.txt'))
+colors = plt.matplotlib.cm.Greens(np.linspace(0.3, 0.7, n_files))
+
+for i, DEMFileName in enumerate(files):
+    pointcp, pointsd = CalculateTaylorPoint(DEMFileName,InitialDEM,RefDod)
+    dia2.add_sample(pointsd, pointcp, marker='o', ms=10, ls='', mfc=colors[i], mec='k')
+
+# D4
+files = sorted(glob(DataDirectory+'D4*.txt'))
+colors = plt.matplotlib.cm.Wistia(np.linspace(0.3, 0.7, n_files))
+
+for i, DEMFileName in enumerate(files):
+    pointcp, pointsd = CalculateTaylorPoint(DEMFileName,InitialDEM,RefDod)
+    dia2.add_sample(pointsd, pointcp, marker='o', ms=10, ls='', mfc=colors[i], mec='k')
+
+
+
+
+# pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS1,InitialDEM,RefDod)
+# dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='r', mec='k')
+#
+# pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS2,InitialDEM,RefDod)
+# dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='g', mec='k')
+#
+# pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS3,InitialDEM,RefDod)
+# dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='b', mec='k')
+#
+# pointcp, pointsd = CalculateTaylorPoint(DataDirectory+Filename_TS4,InitialDEM,RefDod)
+# dia.add_sample(pointsd, pointcp, marker='o', ms=6, ls='', mfc='k', mec='r')
+
+plt.show()
