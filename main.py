a8212e808dbcf3bbc96084ad201fa4184a4b2367
Fiona Clubb <f.clubb@ed.ac.uk>
2018-03-13 12:19:27 +0100

    Reading in the raster data

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 # Script to make a Taylor diagram from LEM inputs
 # Geo.X hackathon 13/03/18
 
-import gdal-io
+import gdalio as gdal_io
+import numpy as np
 
 # read in the raster
-DataDirectory = 'path/to/data/directory/'
-RasterName = 'initial_topography.asc'
-RasterFile = DataDirectory+RasterName
+DataDirectory = '../CAESAR_data/'
+InitialFileName = 'D4elev.dat76212001.txt'
+Filename_TS1 = 'D4elev.dat73584003.txt'
+Filename_TS2 = 'D4elev.dat110376000.txt'
 
-# this is a np.array with the raster data
-RasterData = gdal-io.ReadRasterArrayBlocks(RasterFile,raster_band=1)
+print ("Reading in the raster data...")
+
+# these are np.arrays with the raster data
+InitialTopoArray = gdal_io.ReadRasterArrayBlocks(DataDirectory+InitialFileName,raster_band=1)
+TimeStep1Array = gdal_io.ReadRasterArrayBlocks(DataDirectory+Filename_TS1,raster_band=1)
+TimeStep2Array = gdal_io.ReadRasterArrayBlocks(DataDirectory+Filename_TS2,raster_band=1)
+
+# strip the nans
+InitialTopoArray = InitialTopoArray[np.logical_not(np.isnan(InitialTopoArray))]
+TimeStep1Array = TimeStep1Array[np.logical_not(np.isnan(TimeStep1Array))]
+TimeStep2Array = TimeStep2Array[np.logical_not(np.isnan(TimeStep2Array))]
+
+print len(InitialTopoArray)
+print len(TimeStep1Array)
+print len(TimeStep2Array)
