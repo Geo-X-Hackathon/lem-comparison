6f395ca394dca0a57a730b6710d4ab9c74e2532b
Fiona Clubb <f.clubb@ed.ac.uk>
2018-03-14 12:28:17 +0100

    Adding some more explanation to the functions

--- generate_taylor_diagram.py
+++ generate_taylor_diagram.py
@@ -10,10 +10,15 @@ from correlation import *
 from taylorDiagram import *
 from glob import glob
 
-def SetUpTaylorDiagram(DataDirectory, RefFileName, InitialDEM, RefStd, Normalize=0):
+def SetUpTaylorDiagram(RefStd, Normalize=0):
     """
     Set up the taylor diagram using the intiial DEM and the
     reference DEM
+
+    Args:
+        RefStd (float): the standard deviation of the reference DoD
+        Normalize (int): a switch to choose whether or not to noramlise the standard
+        deviation. 0 = don't noramlise (default); 1 = normalise.
     """
 
     #create figure
@@ -33,9 +38,21 @@ def SetUpTaylorDiagram(DataDirectory, RefFileName, InitialDEM, RefStd, Normalize
 
     return fig, dia
 
-def AddSinglePointToDiagram(DataDirectory,DEMFileName, InitialDEM, RefDod, Normalize=0, color='k', label=' ', markersize=8):
+def AddSinglePointToDiagram(DataDirectory,DEMFileName, InitialDEM, RefDod, RefStd, Normalize=0, color='k', label=' ', markersize=8):
     """
     Function to add a point from a single DEM to the diagram
+
+    Args:
+        DataDirectory (str): the name of the data directory
+        DEMFileName (str): name of the DEM that you want to plot
+        InitialDEM (arr): array of the initial DEM
+        RefDod (arr): array with the DEM of difference for the reference DEM
+        RefStd (float): the standard deviation of the reference DoD
+        Normalize (int): a switch to choose whether or not to noramlise the standard
+        deviation. 0 = don't noramlise (default); 1 = normalise.
+        color (str): the color that you want the point to be, default = black
+        label (str): you can pass a string to the legend for the label. If not passed, then it will just be labelled with the name of the DEM.
+        markersize (int): size of the point
     """
     # check if you want to label the point
     this_label = DEMFileName
@@ -62,13 +79,13 @@ if __name__ == '__main__':
     RefDod, RefStd = ReadReference(InitialDEM,DataDirectory+RefFileName)
 
     # set up the Taylor Diagram
-    fig, dia = SetUpTaylorDiagram(DataDirectory, RefFileName, InitialDEM, RefStd, Normalize)
+    fig, dia = SetUpTaylorDiagram(RefStd, Normalize)
 
     # proof of concept - just add one point to the diagram
     DEMFileName = 'movern_0p35_n_is_two60.bil'
     color = 'blue'
     # now add a point
-    AddSinglePointToDiagram(DataDirectory,DEMFileName, InitialDEM, RefDod, Normalize, color)
+    AddSinglePointToDiagram(DataDirectory,DEMFileName, InitialDEM, RefDod, RefStd, Normalize, color)
 
     # add legend
     fig.legend(dia.samplePoints,
